ca351de7d5b1c7571b7bcc9a351f6cd8c5112b76
21120460 <tanhung126612@gmail.com>
2025-01-12 20:31:43 +0700

    fix builder

--- ValidationFramework/Common/ValidateBuilder.cs
+++ ValidationFramework/Common/ValidateBuilder.cs
@@ -7,9 +7,23 @@ namespace ValidationFramework.Common
     public class ValidateBuilder
     {
         private BaseValidator? _validator;
+        private BaseValidator? _lastValidator; // Lưu validator cuối cùng
 
         public ValidateBuilder() { }
 
+        public void AddValidator(BaseValidator validator)
+        {
+            if (_validator == null)
+            {
+                _validator = validator;
+                _lastValidator = validator; // Nếu là validator đầu tiên, gán cho cả hai
+            }
+            else
+            {
+                _lastValidator?.SetNextValidator(validator); // Thêm vào cuối chuỗi
+                _lastValidator = validator; // Cập nhật validator cuối cùng
+            }
+        }
         public void AddRegexValidator(RegexValidator validator)
         {
             if (_validator == null)
