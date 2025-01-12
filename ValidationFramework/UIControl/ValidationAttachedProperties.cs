456430564e12ec749fdff4d956b624e01c80f192
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-01-12 19:30:00 +0700

    revert

--- ValidationFramework/UIControl/ValidationAttachedProperties.cs
+++ ValidationFramework/UIControl/ValidationAttachedProperties.cs
@@ -1,105 +1,90 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using ValidationFramework.Base;
 using ValidationFramework.Common;
 
-namespace ValidationLib.UIControl
+namespace ValidationLib.UIControl;
+
+public static class ValidationAttachedProperties
 {
-    public static class ValidationAttachedProperties
+    /// <summary>
+    /// Rules attached property
+    /// </summary>
+    public static readonly DependencyProperty RulesProperty =
+        DependencyProperty.RegisterAttached(
+            "Rules",
+            typeof(List<BaseValidateRule>),
+            typeof(ValidationAttachedProperties),
+            new PropertyMetadata(new List<BaseValidateRule>()));
+
+    [Category("Validation")]
+    [Browsable(true)]
+    [DisplayName("Rules")]
+    [AttachedPropertyBrowsableForType(typeof(TextBox))]
+    public static List<BaseValidateRule> GetRules(DependencyObject element)
     {
-        /// <summary>
-        /// Rules attached property
-        /// </summary>
-        public static readonly DependencyProperty RulesProperty =
-            DependencyProperty.RegisterAttached(
-                "Rules",
-                typeof(List<BaseValidateRule>),
-                typeof(ValidationAttachedProperties),
-                new PropertyMetadata(new List<BaseValidateRule>()));
+        return (List<BaseValidateRule>)element.GetValue(RulesProperty);
+    }
 
-        [Category("Validation")]
-        [Browsable(true)]
-        [DisplayName("Rules")]
-        [AttachedPropertyBrowsableForType(typeof(Control))]
-        public static List<BaseValidateRule> GetRules(DependencyObject element)
+
+    /// <summary>
+    /// Handler attached property (hidden)
+    /// </summary>
+    public static readonly DependencyProperty ObserverProperty =
+        DependencyProperty.RegisterAttached(
+            "Observer",
+            typeof(ValidateObserver),
+            typeof(ValidationAttachedProperties),
+            new PropertyMetadata(new ValidateObserver()));
+
+    [Browsable(false)]
+    public static ValidateObserver GetObserver(DependencyObject element)
+    {
+        var handler = (ValidateObserver)element.GetValue(ObserverProperty);
+        var rules = GetRules(element);
+        handler.Rules = rules;
+        return (ValidateObserver)element.GetValue(ObserverProperty);
+    }
+
+
+    /// <summary>
+    /// Trigger attached property
+    /// </summary>
+    public static readonly DependencyProperty TriggerProperty =
+        DependencyProperty.RegisterAttached(
+            "Trigger",
+            typeof(BaseTrigger),
+            typeof(ValidationAttachedProperties),
+            new PropertyMetadata(null, OnTriggerChanged));
+
+    [Category("Validation")]
+    [Browsable(true)]
+    [DisplayName("Trigger")]
+    [AttachedPropertyBrowsableForType(typeof(TextBox))]
+    public static BaseTrigger GetTrigger(DependencyObject element)
+    {
+        return (BaseTrigger)element.GetValue(TriggerProperty);
+    }
+
+    public static void SetTrigger(DependencyObject element, BaseTrigger value)
+    {
+        element.SetValue(TriggerProperty, value);
+    }
+
+    private static void OnTriggerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is TextBox textBox)
         {
-            return (List<BaseValidateRule>)element.GetValue(RulesProperty);
-        }
-
-        /// <summary>
-        /// Observer attached property (hidden)
-        /// </summary>
-        public static readonly DependencyProperty ObserverProperty =
-            DependencyProperty.RegisterAttached(
-                "Observer",
-                typeof(ValidateObserver),
-                typeof(ValidationAttachedProperties),
-                new PropertyMetadata(null));
-
-        [Browsable(false)]
-        public static ValidateObserver GetObserver(DependencyObject element)
-        {
-            // Get or create a unique instance of ValidateObserver for this control
-            var observer = (ValidateObserver)element.GetValue(ObserverProperty);
-            if (observer == null)
+            BaseTrigger? existingValue = GetTrigger(textBox);
+            existingValue?.Detach();
+            if (e.NewValue is BaseTrigger newValue)
             {
-                observer = new ValidateObserver();
-                element.SetValue(ObserverProperty, observer);
-            }
-
-            // Update rules from the control
-            observer.Rules = GetRules(element);
-            return observer;
-        }
-
-        /// <summary>
-        /// Trigger attached property
-        /// </summary>
-        public static readonly DependencyProperty TriggerProperty =
-            DependencyProperty.RegisterAttached(
-                "Trigger",
-                typeof(BaseTrigger),
-                typeof(ValidationAttachedProperties),
-                new PropertyMetadata(null, OnTriggerChanged));
-
-        [Category("Validation")]
-        [Browsable(true)]
-        [DisplayName("Trigger")]
-        [AttachedPropertyBrowsableForType(typeof(Control))]
-        public static BaseTrigger GetTrigger(DependencyObject element)
-        {
-            return (BaseTrigger)element.GetValue(TriggerProperty);
-        }
-
-        public static void SetTrigger(DependencyObject element, BaseTrigger value)
-        {
-            element.SetValue(TriggerProperty, value);
-        }
-
-        private static void OnTriggerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is TextBox textBox)
-            {
-                BaseTrigger? existingValue = GetTrigger(textBox);
-                existingValue?.Detach();
-                if (e.NewValue is BaseTrigger newValue)
-                {
-                    newValue.SetControl(textBox);
-                    newValue.Attach();
-                }
-            }
-            else if (d is PasswordBox passwordBox)
-            {
-                BaseTrigger? existingValue = GetTrigger(passwordBox);
-                existingValue?.Detach();
-                if (e.NewValue is BaseTrigger newValue)
-                {
-                    newValue.SetControl(passwordBox);
-                    newValue.Attach();
-                }
+                newValue.SetControl(textBox);
+                newValue.Attach();
             }
         }
     }
+
 }
--- ValidationFramework/UIControl/ValidationHelper.cs
+++ ValidationFramework/UIControl/ValidationHelper.cs
@@ -1,89 +1,94 @@
-﻿using System.ComponentModel;
+﻿using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using ValidationFramework.Base;
 using ValidationFramework.Common;
 
-namespace ValidationLib.UIControl
+namespace ValidationLib.UIControl;
+
+public class ValidationHelper : ContentControl
 {
-    public class ValidationHelper : ContentControl
+    /// <summary>
+    /// Source attached property
+    /// </summary>
+    public static readonly DependencyProperty TargetProperty =
+        DependencyProperty.Register(
+            nameof(Target),
+            typeof(TextBox),
+            typeof(ValidationHelper),
+            new PropertyMetadata(null, OnTargetChanged));
+
+    [Category("Validation")]
+    [Browsable(true)]
+    public TextBox Target
     {
-        /// <summary>
-        /// Target attached property
-        /// </summary>
-        public static readonly DependencyProperty TargetProperty =
-            DependencyProperty.Register(
-                nameof(Target),
-                typeof(Control),
-                typeof(ValidationHelper),
-                new PropertyMetadata(null, OnTargetChanged));
+        get => (TextBox)GetValue(TargetProperty);
+        set => SetValue(TargetProperty, value);
+    }
 
-        [Category("Validation")]
-        [Browsable(true)]
-        public Control Target
+    private static void OnTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is ValidationHelper helper && helper.Renderer != null)
         {
-            get => (Control)GetValue(TargetProperty);
-            set => SetValue(TargetProperty, value);
-        }
-
-        private static void OnTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is ValidationHelper helper)
+            if (e.OldValue is TextBox oldControl)
             {
-                if (e.OldValue is Control oldControl &&
-                    oldControl.GetValue(ValidationAttachedProperties.ObserverProperty) is ValidateObserver oldObserver)
+                if (oldControl.GetValue(UIControl.ValidationAttachedProperties.ObserverProperty) is ValidateObserver observer)
                 {
-                    oldObserver.Renderer = null;
-                }
+                    observer.Renderer = null;
 
-                if (e.NewValue is Control newControl &&
-                    newControl.GetValue(ValidationAttachedProperties.ObserverProperty) is ValidateObserver newObserver)
+                }
+            }
+            if (e.NewValue is TextBox newControl)
+            {
+                if (newControl.GetValue(UIControl.ValidationAttachedProperties.ObserverProperty) is ValidateObserver observer)
                 {
-                    newObserver.Renderer = helper.Renderer;
+                    observer.Renderer = helper.Renderer;
+
                 }
             }
         }
+    }
 
-        /// <summary>
-        /// Renderer attached property
-        /// </summary>
-        public static readonly DependencyProperty RendererProperty =
-            DependencyProperty.Register(
-                nameof(Renderer),
-                typeof(BaseValidationResultRenderer),
-                typeof(ValidationHelper),
-                new PropertyMetadata(null, OnRendererChanged));
+    /// <summary>
+    /// Display attached property
+    /// </summary>
+    public static readonly DependencyProperty RendererProperty =
+        DependencyProperty.Register(
+            nameof(Renderer),
+            typeof(BaseValidationResultRenderer),
+            typeof(ValidationHelper),
+            new PropertyMetadata(null, OnRendererChanged));
 
-        [Category("Validation")]
-        [Browsable(true)]
-        public BaseValidationResultRenderer Renderer
+    [Category("Validation")]
+    [Browsable(true)]
+    public BaseValidationResultRenderer Renderer
+    {
+        get => (BaseValidationResultRenderer)GetValue(RendererProperty);
+        set => SetValue(RendererProperty, value);
+    }
+
+    private static void OnRendererChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is ValidationHelper helper)
         {
-            get => (BaseValidationResultRenderer)GetValue(RendererProperty);
-            set => SetValue(RendererProperty, value);
-        }
-
-        private static void OnRendererChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is ValidationHelper helper)
+            if (e.OldValue is BaseValidationResultRenderer oldRenderer)
             {
-                if (e.OldValue is BaseValidationResultRenderer oldRenderer)
+                oldRenderer.RemoveValidationHelper();
+                if (helper.Target?.GetValue(UIControl.ValidationAttachedProperties.ObserverProperty) is ValidateObserver observer)
                 {
-                    oldRenderer.RemoveValidationHelper();
-                    if (helper.Target?.GetValue(ValidationAttachedProperties.ObserverProperty) is ValidateObserver oldObserver)
-                    {
-                        oldObserver.Renderer = null;
-                    }
+                    observer.Renderer = null;
                 }
-
-                if (e.NewValue is BaseValidationResultRenderer newRenderer)
+            }
+            if (e.NewValue is BaseValidationResultRenderer newRender)
+            {
+                newRender.AddValidationHelper(helper);
+                if (helper.Target?.GetValue(UIControl.ValidationAttachedProperties.ObserverProperty) is ValidateObserver observer)
                 {
-                    newRenderer.AddValidationHelper(helper);
-                    if (helper.Target?.GetValue(ValidationAttachedProperties.ObserverProperty) is ValidateObserver newObserver)
-                    {
-                        newObserver.Renderer = newRenderer;
-                    }
+                    observer.Renderer = newRender;
                 }
             }
         }
     }
 }
+
